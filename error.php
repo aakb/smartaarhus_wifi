9e38e53e7c41fa956089800571cb912a9af3475c
Steen Larsen <sgl@aarhus.dk>
2014-02-18 14:21:16 +0100

    Adding error-page

---
+++ error.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Brug af cookies</title>
+  <?php include('inc/meta.inc'); ?>
+</head>
+<body>
+
+<?php include('inc/header.inc'); ?>
+<div class="layout">
+  <h1>Fejl</h1>
+  <div class="page-content">
+  
+      <p class="error">%TIDY[error_message]&nbsp;</p>
+      <div class="form-wrapper">
+        <div class="form-item submit">
+          <input type="button" value="Tilbage" id="submit" onclick="javascript:history.back(1);" />
+        </div>
+      </div>    
+    
+  </div>
+  <?php include('inc/footer.inc'); ?>
+</div>
+</body>
+</html>
