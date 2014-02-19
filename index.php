7a45475b83970681656cb58e640bb2e74a023245
Steen Larsen <sgl@aarhus.dk>
2014-02-19 17:10:46 +0100

    Adding skolelogin-page

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   <h1>Vælg login</h1>
   <div class="link-button--wrapper">
     <a href="bibliotekslogin.php" class="link-button">Bibliotekslogin<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
-<!--    <a href="skolelogin.php" class="link-button">Skolelogin<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a> -->
+    <a href="skolelogin.php" class="link-button">Skolelogin<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
     <a href="medarbejderlogin.php" class="link-button">Medarbejderlogin<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
     <a href="sms-send-code.php" class="link-button">SMS login<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
 
