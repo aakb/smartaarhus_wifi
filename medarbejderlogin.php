6d79bac64e0c02dc9e8f86cf60c29e4378d008b1
Steen Larsen <sgl@aarhus.dk>
2014-02-20 15:27:58 +0100

    Minor changes in text

--- medarbejderlogin.php
+++ medarbejderlogin.php
@@ -11,7 +11,7 @@
   <h1>Medarbejderlogin</h1>
   <div class="message--cookie js-cookie-message">
     <p class="message--cookie-saved js-cookie-message-saved">Vil du viderestilles til dette login næste gang du skal logge ind? <a href="#" class="js-save-login-choice">Ja tak, gem mit loginvalg</a></p>
-    <p class="message--cookie-not-saved js-cookie-message-not-saved">Du har gemt dette som dit login valg. <a href="#" class="js-delete-login-choice">Slet mit loginvalg</a></p>
+    <p class="message--cookie-not-saved js-cookie-message-not-saved">Du har gemt dette som dit loginvalg. <a href="#" class="js-delete-login-choice">Slet loginvalg</a></p>
     <a href="#" class="js-hide-message">Vis ikke denne besked igen</a>
   </div>
   <div class="form--wrapper">
