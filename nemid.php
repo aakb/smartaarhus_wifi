2e263deb91b5ce8625777a7c98203aa2418a2879
Steen Larsen <sgl@aarhus.dk>
2014-02-20 10:52:41 +0100

    Adding tidy commands

--- nemid.php
+++ nemid.php
@@ -5,12 +5,17 @@
   <?php include('inc/meta.inc'); ?>
 </head>
 <body>
-
+%TIDY[nemid_javascript]
 <?php include('inc/header.inc'); ?>
 <div class="layout">
-  <h1>Nemid</h1>
+  <h1>Adgang med Nemid</h1>
+  <p>Log ind med dit NemID.</p>
   <div class="form--wrapper">
-...
+        <form name="logonForm" action="%TIDY[form_action]" method="post">
+%TIDY[form_intercept]
+%TIDY[nemid_form]
+%TIDY[nemid_applet]
+  	</form>
   </div>
   <?php include('inc/footer.inc'); ?>
 </div>
