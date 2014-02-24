b7406c3b54c2dd52960bfb04089d4c9be29ad321
Brian Jensen <hallo@magnify.dk>
2014-02-24 16:07:12 +0100

    Added message examples

--- bibliotekslogin.php
+++ bibliotekslogin.php
@@ -13,19 +13,11 @@
     <form action="logged-in.php" method="post">
       <div class="form--password-wrapper">
         <label for="username">Lånerkortnummer / CPR-nummer</label>
-        <input type="password" placeholder="Indtast dit lånerkort- eller CPR-nummer" id="username" name="username" class="form--input" autocomplete="off" />
-        <!--<div class="form--toggle-password js-form-toggle-password" data-toggle-password="username">
-          <img src="/public/eye.png" class="form--toggle-password-icon" />
-          <span class="js-form-toggle-text">Vis</span>
-        </div>-->
+        <input type="password" placeholder="Indtast dit lånerkort- eller CPR-nummer" id="username" name="username" class="form--input" autocomplete="off" required />
       </div>
       <div class="form--password-wrapper">
         <label for="password">Pinkode</label>
-        <input type="password" placeholder="Indtast din pinkode" id="password" name="password" class="form--input" autocomplete="off" />
-        <!--<div class="form--toggle-password js-form-toggle-password" data-toggle-password="password">
-          <img src="/public/eye.png" class="form--toggle-password-icon" />
-          <span class="js-form-toggle-text">Vis</span>
-        </div>-->
+        <input type="password" placeholder="Indtast din pinkode" id="password" name="password" class="form--input" autocomplete="off" required />
       </div>
       <input type="submit" class="button" value="Log ind" />
     </form>
