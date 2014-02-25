a57c50e148e81b31d285aaec459e2dea5094fd27
Brian Jensen <hallo@magnify.dk>
2014-02-25 15:14:34 +0100

    Updated text string

--- en_cookies.php
+++ en_cookies.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Cookies usage - english version</title>
+  <title>Use of cookies - english version</title>
   <?php include('inc/meta.inc'); ?>
 </head>
 <body>
 
 <?php include('inc/header.inc'); ?>
 <div class="layout">
-  <h1>Cookies usage</h1>
+  <h1>Use of cookies</h1>
   <div class="page-content">
     <p>[Cookie tekst]</p>
-    <p class="js-delete-cookies"><a href="#" class="js-delete-cookies-link">Slet cookies</a></p>
   </div>
   <?php include('inc/en_footer.inc'); ?>
 </div>
