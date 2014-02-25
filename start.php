5ce19d9b5ddd61133cd8410b208b4b559e20cab7
Steen Larsen <sgl@aarhus.dk>
2014-02-25 11:11:05 +0100

    Adding internal page to test

---
+++ start.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+  <title>SmartAarhus WIFI</title>
+  <?php include('inc/meta.inc'); ?>
+</head>
+
+<body>
+<script>redirectToLogin();</script>
+<div>
+  <?php include('inc/header.inc'); ?>
+  <div class="layout">
+    <h1>Internal page</h1>
+    <p>Denne side er ikke del af templates.
+    <div class="link-button--wrapper">
+      <p><a target="tabs" href="http://nwa-demo.netic.dk/">Test</a>
+      <p><a target="tabs" href="http://nwa-demo.netic.dk/auth/method/?intercept_uri=687474703A2F2F7777772E6F6461612E646B2F">Test med intercepturi</a>
+    </div>
+          <p class="js-delete-cookies"><a href="#" class="js-delete-cookies-link">Slet cookies</a></p>
+  </div>
+
+</div>
+</body>
+</html>
