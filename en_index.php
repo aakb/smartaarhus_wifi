90e7a02625e0a2c3408490688f8db1f81fc8a1d0
Brian Jensen <hallo@magnify.dk>
2014-02-25 14:28:17 +0100

    Updated index buttons

--- en_index.php
+++ en_index.php
@@ -11,12 +11,12 @@
   <div class="layout">
     <h1>Choose login method</h1>
     <div class="link-button--wrapper">
-      <a href="bibliotekslogin.php" class="link-button">Bibliotekslogin<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
-      <a href="skolelogin.php" class="link-button">Skolelogin<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
-      <a href="medarbejderlogin.php" class="link-button">Medarbejderlogin<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
+      <a href="bibliotekslogin.php" class="link-button">Library login<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
+      <a href="skolelogin.php" class="link-button">School login<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
+      <a href="medarbejderlogin.php" class="link-button">Employee login<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
       <a href="sms-send-code.php" class="link-button">SMS login<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
 
-      <a href="guest.php" class="link-button">G&aelig;stelogin<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
+      <a href="guest.php" class="link-button">Guest login<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
       <a href="nemid.php" class="link-button">Nemid<span class="link-button--icon-wrapper"><img src="/public/arrow-right.png" class="link-button--icon" /></span></a>
           
       
