c6fd80f45edaf67e8fed81280d3d21aab6d8d6a8
Brian Jensen <hallo@magnify.dk>
2014-02-26 12:25:44 +0100

    Updated cookie texts

--- cookies.php
+++ cookies.php
@@ -12,7 +12,7 @@
   <div class="page-content">
     <p>Dette website benytter cookies til at gemme dit valg af sprog og til at gemme din foretrukne loginmetode (f.eks. Medarbejder login).</p>
     <p>De fornævnte cookies er undtaget af cookieloven, men for en god ordens skyld har vi valgt at informere om brugen alligevel.</p>
-    <p>Det er ikke muligt at benytte denne service uden at acceptere brugen af sprog cookien. Cookien til loginmetode bliver kun gemt hvis du aktivt vælger at gemme dit login.</p>
+    <p>Det er ikke muligt at benytte denne service uden at acceptere brugen af sprog cookien. Cookien til loginmetode bliver kun gemt hvis du vælger at gemme dit login.</p>
   </div>
   <?php include('inc/footer.inc'); ?>
 </div>
--- en_cookies.php
+++ en_cookies.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Use of cookies - english version</title>
+  <title>Use of cookies</title>
   <?php include('inc/meta.inc'); ?>
 </head>
 <body>
@@ -10,7 +10,9 @@
 <div class="layout">
   <h1>Use of cookies</h1>
   <div class="page-content">
-    <p>[Cookie tekst]</p>
+    <p>This website uses cookies to store your choice language and to store your preferred access method (eg. Employee login).</p>
+    <p>The before mentioned cookies are exempt from the cookie law, but for good measure we have decided to inform about the use anyway.</p>
+    <p>It is not possible to use this service without accepting the use of the language cookie. The cookie for login method is optional and will only be stored if you choose to save your login.</p>
   </div>
   <?php include('inc/en_footer.inc'); ?>
 </div>
