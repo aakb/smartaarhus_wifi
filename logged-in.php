d585a5d79cbf5471c3480b3809d0ca391946ed23
Steen Larsen <sgl@aarhus.dk>
2014-02-26 11:16:46 +0100

    Correct tidy-commands

--- logged-in.php
+++ logged-in.php
@@ -10,10 +10,10 @@
 <div class="layout">
   <h1>Du er nu online</h1>
 
-  %TIDY_DEP[redirect] %TIDY_IFDEF[intercept_uri]  <div class="message--success">
-  %TIDY_DEP[redirect] %TIDY_IFDEF[intercept_uri]    <p>Du bliver videresendt til din destination om 5 sekunder.</p>
-  %TIDY_DEP[redirect] %TIDY_IFDEF[intercept_uri]    <p><a href="%TIDY[intercept_uri]">%TIDY[intercept_uri_nice]</a></p>
-  %TIDY_DEP[redirect] %TIDY_IFDEF[intercept_uri]  </div>
+%TIDY_DEP[redirect] %TIDY_IFDEF[intercept_uri]  <div class="message--success">
+%TIDY_DEP[redirect] %TIDY_IFDEF[intercept_uri]    <p>Du bliver videresendt til din destination om 5 sekunder.</p>
+%TIDY_DEP[redirect] %TIDY_IFDEF[intercept_uri]    <p><a href="%TIDY[intercept_uri]">%TIDY[intercept_uri_nice]</a></p>
+%TIDY_DEP[redirect] %TIDY_IFDEF[intercept_uri]  </div>
 
   <p>Denne session udl&oslash;ber <strong>%TIDY[expire_time]</strong>, herefter vil du igen blive bedt om at logge ind.</p>
   <h3>Sikkerhed p&aring; nettet</h3>
