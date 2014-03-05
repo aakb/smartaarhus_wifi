a2ac0b5432a07e6f65dc582d6926e2725ed5f5f1
bombycilla <sgl@aarhus.dk>
2014-03-05 18:51:18 +0100

    Reorder script

--- index.php
+++ index.php
@@ -2,41 +2,57 @@
 
 $sub_directory ='aarhus/';
 
+$usernames = array('0000');
+$smsPhase2Index = 'sms_intermediate_page';
+$loggedIndex = 'logged-in';
+
 $mapping  = array(
 
-              'index'   => array('link'=>'auth_home_link','file'=>'auth_methods'),
+  'index' => array('link' => 'auth_home_link', 'file' => 'auth_methods'),
+  'bibliotekslogin' => array('link' => 'auth_axiell_link', 'file' => 'auth_axiell_login' ),
+  'guest' => array('link' => 'auth_nwa_link', 'file' => 'auth_nwa_login'),
+  'medarbejderlogin' => array('link' => 'auth_ads_link', 'file' => 'auth_ads_login' ),
+  'nemid' => array('link' => 'auth_nemid_link', 'file' => 'auth_nemid_login' ),
+  'skolelogin' => array('link' => 'auth_radius_link', 'file' => 'auth_radius_login' ),
+  'sms-login' => array('link' => 'auth_sms_link', 'file' => 'auth_sms_login', 'sms' => 1 ),
+  'sms-send-code' => array('link' => $smsPhase2Index, 'file' => 'auth_sms_login', 'sms' => 2  ),
 
-              'error'   => array('file'=>'error' ),
+  'error' => array('file' => 'error' ),
+  'logged-in' => array('link' => $loggedIndex, 'file' => 'host_active' ),
+);
 
-              'bibliotekslogin'   => array('link'=>'auth_axiell_link','file'=>'auth_axiell_login' ),
+$link_mapping = array(
 
-              'guest'   => array('link'=>'auth_nwa_link','file'=>'auth_nwa_login'),
+  // The Action url.
+  'form_action' => '/auth/=/logon/',
 
-              'medarbejderlogin'   => array('link'=>'auth_ads_link','file'=>'auth_ads_login' ),
+  // Hide this
+  'form_intercept' => '',
+  'redirect' => '',
+  'sms_form' => '',
+  'sms_notice' => '',
 
-              'nemid'   => array('link'=>'auth_nemid_link','file'=>'auth_nemid_login' ),
+  // Footer language-link
+  'language_en_js' => 'javascript:set_language(&#39;en&#39;); void(0);',
+  'language_da_js' => 'javascript:set_language(&#39;da&#39;); void(0);',
 
-              'skolelogin'   => array('link'=>'auth_radius_link','file'=>'auth_radius_login' ),
+  // DEMO values
+  'sms_number' => '12345678',
+  'intercept_uri' => 'http://www.aakb.dk',
+  'intercept_uri_nice' => 'http://www.aakb.dk',
+  'expire_time' => 'Onsdag kl. 17',
 
-              'sms-login'   => array('link'=>'auth_sms_link','file'=>'auth_sms_login', 'sms' => 1 ),
+);
 
-              'sms-send-code'   => array('file'=>'auth_sms_login', 'sms' => 2  ),
-
-              'logged-in' => array('file'=>'host_active' ),
-  );
-
-$correct_username = '0000';
-  
-  
-$reverse_mapping = array();
 foreach ($mapping as $key => $value) {
-    if (  isset($value['link'])   ) {
-        $reverse_mapping[ $value['link'] ] =  $key .'.php';
-    }
+  if (  isset($value['link'])   ) {
+    $link_mapping[ $value['link'] ] =  '/' . $key .'.php';
+  }
 }
 
 function remove_tidy_dep(&$content) {
-  $content = preg_replace('/^\%TIDY_DEP\[.*?\]\s*/m','', $content);
+  // Remove TIDY_DEP.
+  $content = preg_replace('/^\%TIDY_DEP\[.*?\]\s*/m', '', $content);
 }
 
 function load_file($file) {
@@ -51,7 +67,7 @@ function load_file($file) {
 
 function handle_include(&$content) {
   $content = preg_replace_callback(
-      '/\%TIDY\[include\(([\w\_\.]+)\)\]/', 
+      '/\%TIDY\[include\(([\w\_\.]+)\)\]/',
       function($matches){
           return load_file($matches[1]);
       },
@@ -61,9 +77,9 @@ function handle_include(&$content) {
 
 function handle_link(&$content, $links) {
 
-  // fjern alle %TIDY_IFDEF[auth_axiell_link]
-  //$content = preg_replace('/^\%TIDY_IFDEF\[.*?\]/m','', $content);
-  $content = preg_replace('/\%TIDY_IFDEF\[.*?\]/m','', $content);
+  // fjern alle %TIDY_IFDEF[...]
+  //$content = preg_replace('/^\%TIDY_IFDEF\[.*?\]/m', '', $content);
+  $content = preg_replace('/\%TIDY_IFDEF\[.*?\]/m', '', $content);
 
   // erstat link hvis link findes
   $content = preg_replace_callback(
@@ -73,90 +89,75 @@ function handle_link(&$content, $links) {
       },
       $content
       );
-
 }
 
 function extract_headers(&$content, &$headers){
-  $headers_arr=array();
-
+  // Extract all headers from content.
   $tmp = preg_replace_callback(
       '/^\%TIDY_PAGEHEAD: (.*)$/m',
-      function($matches) use (&$headers_arr) {
-        $headers_arr[] = $matches[1];
+      function($matches) use (&$headers) {
+        $headers .= $matches[1] . "\n";
         return '';
       },
       $content
       );
       
-  $content = preg_replace('/^\s+/s', '', $tmp);
-  $headers = implode("\n", $headers_arr);
+  // Small cleanup.
+  $content = preg_replace('/^\s+$/m', '', $tmp);
 }
+
 // find language
 $language = $_COOKIE["tidyLanguage"] === 'en' ? 'en' : 'da';
 
-// handle login/post with json response - url contains '='
+// handle login/post with json response - url will contain '='
 if ( isset($_SERVER["REDIRECT_URL"]) && preg_match("/\=/", $_SERVER["REDIRECT_URL"])   ){
 
-    header('Content-type:application/json');
-    if( $_REQUEST['username'] == $correct_username) {
-        header('HTTP/1.0 200 Found');
-        echo '{"status":"0","site_id":"23","authenticated":1,"username":"...","host":"...","redirect":"/logged-in.php","method":"...","expire":"7200"}';
-    } else {
-        header('HTTP/1.0 400 Bad Request');
-        echo '{"status":"validation failed","site_id":"23","method":"ads","authenticated":0,"expire":0,"message":"Forkert brugernavn eller adgangskode.","host":"..."}';
-    }
-    exit;
+  header('Content-type:application/json');
+  if( in_array($_REQUEST['username'], $usernames)) {
+    header('HTTP/1.0 200 Found');
+    echo '{"status":"0","authenticated":1,"redirect":"' . $link_mapping[$loggedIndex] . '"}';
+  } else {
+    header('HTTP/1.0 400 Bad Request');
+    $message = array('da' => 'Forkert brugernavn eller adgangskode.', 'en' => 'Wrong username or password');    
+    echo '{"status":"validation failed","authenticated":0,"message":"' . $message[$language] . '"}';
+  }
+  exit;
 }
 
 // find filename
-$filename = isset($_SERVER["REDIRECT_URL"]) ? $_SERVER["REDIRECT_URL"] : 'index.php';
-$filename = preg_replace('~^/~', '', $filename);
-$filename = preg_replace('/\.php/i', '', $filename);
-
+if ( isset($_SERVER["REDIRECT_URL"]) && preg_match('~^/(.*?)\.php~', $_SERVER["REDIRECT_URL"], $matches)) {
+  $fileIndex = $matches[1];
+} else {
+  $fileIndex = 'index';
+}
 
 // get the right file
-$localfile = $mapping[$filename]['file'] . '_' . $language . '.txt';
+$localfile = $mapping[$fileIndex]['file'] . '_' . $language . '.txt';
 
 $content = load_file($localfile);
 
-// start parsing
+// handle include-files
 handle_include($content);
-
-$reverse_mapping['form_action']='/auth/=/logon/';
-$reverse_mapping['form_intercept']='';
-$reverse_mapping['redirect']='';
-
-$reverse_mapping['intercept_uri']='http://www.aakb.dk';
-$reverse_mapping['intercept_uri_nice']='http://www.aakb.dk';
-
-$reverse_mapping['expire_time']='Onsdag kl. 17';
-
-
 
 // handle footer link
 $content = preg_replace('/\%TIDY_REGEX\[language_modules.*?\]/', '', $content);
-$reverse_mapping['language_en_js']='javascript:set_language(&#39;en&#39;); void(0);';
-$reverse_mapping['language_da_js']='javascript:set_language(&#39;da&#39;); void(0);';
 
-// handle sms
-if ( isset($mapping[$filename]['sms'])){
+// handle sms-pages
+if ( isset($mapping[$fileIndex]['sms'])){
 
-  $reverse_mapping['sms_form']='';
-  $reverse_mapping['sms_notice']='';
-  
-  if ( $mapping[$filename]['sms'] == 1 ) {
-    $content = preg_replace('/^\%TIDY_IFDEF\[sms_sent\].*/m','', $content);
-    $reverse_mapping['form_action']='/sms-send-code.php';
-  } else if ( $mapping[$filename]['sms'] == 2 ) {
-    $content = preg_replace('/^\%TIDY_IFDEF\[\!sms_sent\].*/m','', $content);
-    $reverse_mapping['form_action']='/logged-in.php';    
-    $reverse_mapping['sms_number']='12345678';   
+  if ( $mapping[$fileIndex]['sms'] == 1 ) {
+    $content = preg_replace('/^\%TIDY_IFDEF\[sms_sent\].*/m', '', $content);
+    $link_mapping['form_action'] = $link_mapping[$smsPhase2Index];
+  } else if ( $mapping[$fileIndex]['sms'] == 2 ) {
+    $content = preg_replace('/^\%TIDY_IFDEF\[\!sms_sent\].*/m', '', $content);
+    $link_mapping['form_action'] = $link_mapping[$loggedIndex];
   }
 }
 
 // handle link
-handle_link($content, $reverse_mapping);
+handle_link($content, $link_mapping);
 
+// handle headers
 $headers='';
 extract_headers($content, $headers);
 
@@ -172,5 +173,4 @@ extract_headers($content, $headers);
 <div class="main_wrapper">
     <?php echo $content; ?>
 </div>
-<div>Demo: Brug <?php echo $correct_username;?> som testbrugernavn.</div>
 </body></html>
